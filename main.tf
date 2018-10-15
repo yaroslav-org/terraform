b376cfcf4dde8692abac3165451db840644f66e2
yaroslav-007 <43340595+yaroslav-007@users.noreply.github.com>
2018-10-15 11:11:27 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-resource "null_resource" "helloWorld" {
+resource "null_resource" "CheckNorris" {
+  provisioner "local-exec" {
+    command = "echo hello world"
+  }
+}
+
+resource "null_resource" "byeWorld" {
   provisioner "local-exec" {
     command = "echo hello world"
   }
